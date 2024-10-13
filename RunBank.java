2aa262fb894d9411c23cd3c50c8803974ab9ca56
V00D00M <104918488+V00D00M@users.noreply.github.com>
2024-10-12 23:50:23 -0600

    Adding files to repo

---
+++ RunBank.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.io.*;
+import java.util.Scanner;
+import java.util.ArrayList;
+
+
+// main method of entire project goes here
+public class RunBank {
+    public static void main (String args[]) {
+        //hello there
+    }
+}
