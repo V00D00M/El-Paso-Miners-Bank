8bab85e136c1d9a29651766bd716ee9f3f1df011
V00D00M <104918488+V00D00M@users.noreply.github.com>
2024-10-22 00:16:41 -0600

    New Stuff

--- Person.java
+++ Person.java
@@ -1,7 +1,7 @@
 import java.util.List;
 import java.util.ArrayList;
 
-public class Person {
+public abstract class Person {
     private String name;
     private String address;
     private List<Account> accounts;
--- RunBank.java
+++ RunBank.java
@@ -17,36 +17,43 @@ public class RunBank {
             System.out.println("4. Transfer Money Between Accounts");
             System.out.println("5. Make a Payment");
             System.out.println("6. Admin Console");
-            System.out.println("7. Exit");
+            System.out.println("Exit\n");
+            System.out.print("Enter your choice: ");
 
+            String input = sc.next();
 
-            int choice = sc.nextInt();
-            switch (choice) {
-                case 1:
-                    System.out.println("Inquire Account Balance");
-                    break;
-                case 2:
-                    System.out.println("Make a Deposit");
-                    break;
-                case 3:
-                    System.out.println("Make a Withdrawal");
-                    break;
-                case 4:
-                    System.out.println("Transfer Money Between Accounts");
-                    break;
-                case 5:
-                    System.out.println("Make a Payment");
-                    break;
-                case 6:
-                    System.out.println("Admin Console");
-                    break;
-                case 7:
-                    System.out.println("Thank you for choosing El Paso Miners Bank. Goodbye!");
-                    exit = true;
-                    break;
-                default:
-                    System.out.println("Invalid choice. Please try again.");
-                    break;
+            if (input.equalsIgnoreCase("EXIT")) {
+                System.out.println("\nThank you for choosing El Paso Miners Bank. Goodbye!\n");
+                exit = true;
+            } else {
+                try {
+                    int choice = Integer.parseInt(input);
+                    switch (choice) {
+                        case 1:
+                            System.out.println("Inquire Account Balance");
+                            break;
+                        case 2:
+                            System.out.println("Make a Deposit");
+                            break;
+                        case 3:
+                            System.out.println("Make a Withdrawal");
+                            break;
+                        case 4:
+                            System.out.println("Transfer Money Between Accounts");
+                            break;
+                        case 5:
+                            System.out.println("Make a Payment");
+                            break;
+                        case 6:
+                            System.out.println("Admin Console");
+                            break;
+                        default:
+                            System.out.println("Invalid choice. Please try again.");
+                            break;
+                    }
+                } catch (NumberFormatException e) {
+                    System.out.println("\nInvalid choice. Please try again.\n");
+                }
             }
         }
     } 
